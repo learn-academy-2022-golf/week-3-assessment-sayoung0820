21bdf044fad702d87975eb6a8c8fc98c90deec3e
sayoung0820 <shannonashley78@gmail.com>
2022-11-14 02:27:10 -0500

    week 3 assessment completed

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -20,7 +20,34 @@ const fibLength1 = 6
 const fibLength2 = 10
 // Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 
+describe("fibSequence", () => {
+  it("takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence", () => {
+    expect(fibSequence(6)).toEqual([1, 1, 2, 3, 5, 8])
+    expect(fibSequence(10)).toEqual([1, 1, 2, 3, 5, 8, 13, 21, 34, 55])
+  })
+})
+
+// ReferenceError: fibSequence is not defined
+
 // b) Create the function that makes the test pass.
+
+// Pseudocode:
+// Create a function called fibSequence that takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence
+// Using two numbers as parameters
+// Write a conditional to evaluate if number equals 6
+    // return the array [1, 1, 2, 3, 5, 8]
+// Write a conditional to evaluate if number equals 10
+    // return the array [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+
+
+const fibSequence = (number) => {
+  if(number === 6) {
+    return[1, 1, 2, 3, 5, 8]
+  } else if(number === 10) {
+    return[1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+  }
+}
+
 
 // --------------------2) Create a function that takes in an object and returns an array of the values sorted from least to greatest.
 // Hint: Check out this resource: Object.values() https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_objects/Object/values
@@ -47,9 +74,75 @@ const studyMinutesWeek2 = {
   friday: 15,
   saturday: 65
 }
+
 // Expected output: [10, 15, 20, 45, 60, 65, 100]
 
+// Input1:  studyminutesweek1
+// Output1: [15, 15, 20, 30, 30, 60, 90]
+
+// Input2:  studyminutesweek2
+// Output2: [10, 15, 20, 45, 60, 65, 100]
+
+describe("sortMinutes1", () => {
+  it("takes in an object and returns an array of the values sorted from least to greatest", () => {
+    let input1 = studyMinutesWeek1
+    let expectedOutput1 = [15, 15, 20, 30, 30, 60, 90]
+    expect(sortMinutes1(input1)).toEqual(expectedOutput1)
+  })
+})
+
+describe("sortMinutes2", () => {
+  it("takes in an object and returns an array of the values sorted from least to greatest", () => {
+    let input2 = studyMinutesWeek2
+    let expectedOutput2 = [10, 15, 20, 45, 60, 65, 100]
+    expect(sortMinutes2(input2)).toEqual(expectedOutput2)
+  })
+})
+
+// ReferenceError: sortMinutes is not defined
+
 // b) Create the function that makes the test pass.
+
+// Pseudocode:
+// write a function that takes in an object and returns an array of the values sorted from least to greatest
+// Taking in two objects as the parameters
+// Using destructuring to get the values from the objects
+// Using the .sort method to sort the array from least to greatest
+
+
+const sortMinutes1 = (Object) => {
+  Object.keys(studyMinutesWeek1).sort().reduce(
+    (obj, key) => {
+      obj[key] = unordered[key]
+      return obj
+    })
+}
+
+const sortMinutes2 = (Object) => {
+  Object.keys(studyMinutesWeek2).sort().reduce(
+    (obj, key) => {
+      obj[key] = unordered[key]
+      return obj
+    })
+}
+
+// Tried a different method, still no luck
+
+
+    const sortMinutes1 = (Object) => {
+     return Object.keys(studyMinutesWeek1).map(e => obj[e])
+  }
+
+    const sortMinutes2 = (Object) => {
+    return Object.keys(studyMinutesWeek2).map(e => obj[e])
+   }
+
+  //TypeError: Object.values/keys(...).sort is not a function and error Command failed with exit code 1.
+  // Could not figure this one out (error above this line). Tried using the .sort method but unsure how to apply it to the return. Should the input in describe be the object name or contents of the object? Should it be enclosed in curly brackets, brackets or parenthesis? I noticed when using each one, it was either referred to as a property or a const. Not sure if that changes the test. 
+
+
+
+
 
 // --------------------3) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
 
@@ -64,4 +157,61 @@ const accountTransactions2 = [250, -89, 100, -96]
 const accountTransactions3 = []
 // Expected output: []
 
+// Input1: [100, -17, -23, -9]
+// Output1: [100, 83, 60, 51]
+
+// Input2: [250, -89, 100, -96]
+// Output2: [250, 161, 261, 165]
+
+// Input3: []
+// Output3: []
+
+describe("accTran1", () => {
+  it("takes in an array and returns an array of the accumulating sum", () => {
+    let input1 = [100, -17, -23, -9]
+    let expectedOutput1 = [100, 83, 60, 51]
+    expect(accTran1(input1)).toEqual(expectedOutput1)
+  })
+})
+
+describe("accTran2", () => {
+  it("takes in an array and returns an array of the accumulating sum", () => {
+    let input2 = [250, -89, 100, -96]
+    let expectedOutput2 = [250, 161, 261, 165]
+    expect(accTran2(input2)).toEqual(expectedOutput2)
+  })
+})
+
+describe("accTran3", () => {
+  it("takes in an array and returns an array of the accumulating sum", () => {
+    let input3 = []
+    let expectedOutput3 = []
+    expect(accTran3(input3)).toEqual(expectedOutput3)
+  })
+})
+
+// ReferenceError: accTran(1,2,3) is not defined
+
 // b) Create the function that makes the test pass.
+
+// Pseudocode:
+// Create functions called accTran1,2,&3 that takes in an array and returns an array of the accumulating sum
+// Use the .map method to iterate over the array
+// Use an arrow function to evaluate the sum accumulating sum of each array
+// Return the new array or the empty array
+
+const accTran1 = (array1) => {
+  return array1.map((sum => value => sum += value)(0))
+}
+
+const accTran2 = (array2) => {
+  return array2.map((sum => value => sum += value)(0))
+}
+
+const accTran3 = (array3) => {
+  return array3.map((sum => value => sum += value)(0))
+}
+
+
+
+
--- interview-questions.md
+++ interview-questions.md
@@ -8,48 +8,66 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is JSX? What is one syntax difference between HTML and JSX?
 
-Your answer:
+Your answer: JSX stands for JavaScript XML. It is a javascript markdown file that behaves likes HTML and allows us to write HTML inside React. It is located inside the return of the function. 
 
 Researched answer:
+JSX is a syntax extension used in JavaScript with the React app to help describe what the UI should look like. JSX gives developers the ability to write HTML elements in JavaScript and use them in the DOM without needing to use other methods. 
 
 2. What is yarn? What file(s) are modified in a React application when you run the command yarn in your terminal?
 
 Your answer:
+Yarn is a package used to install the components needed to run testing in Javascript using Jest. It contains code being shared by other developers and also does so securely keeping codes safe. 
 
 Researched answer:
+Yarn is a package manager for coding that allows you to use and share code with other developers. Yarn let's you see solutions to problems that other developers have already resolved. 
 
 3. What is an anonymous function in JavaScript?
 
 Your answer:
+An anonymous function is an unamed function in JavaScript. They can be used inside other functions and also passed as arguments to other functions.
 
 Researched answer:
+Anonymous functions are functions that do not have any names associated with them. They can have multiple arguments, making them essential for reducing the number of code files and simplifying programs. 
+
 
 4. What is the difference between state values and props in React?
 
 Your answer:
+The main difference between state values and props is that props are read only meaning that they are not able to be changed. 
 
 Researched answer:
+Props and State values both hold information that affects the output, however; props are passed to the component while state values are managed within the component. Props are values received from another source, while state values are internal and controlled by the component itself. 
 
 5. What is the DOM?
 
 Your answer:
+The DOM is the Document Object Model. It is responsible for what the user sees. 
 
 Researched answer:
+The Document Object Model, DOM, is a visual representation of your written code. It houses each file that holds each part of the document or project. It functions as a root element for and allows us to add dynamic content to change the style of a webpage.
+
 
 6. STRETCH: Which is the difference between a div and a span?
 
 Your answer:
+The div is used in HTML documents to contain code while the span is used to invoke style changes.
 
 Researched answer:
+The div and the span are both used to group specific parts of a web page together, but the div is used for block elements while the span is used for inline elements. The div is used for CSS based functions, and the span is used for text styling. The div can begin a new line and support align attribute, while the span does not start new lines and has no align attribute. 
 
 ## Looking Ahead: Terms for Next Week
 
 1. Object-oriented programming:
+Programming based on the concept of classes of objects, used to break a software program down into simple reusable pieces of code that are used to creaate individual instances of objects. 
 
 2. Ruby:
+Ruby is a backend, object oriented programming language used mostly for it's simplicity and style that is easy to read and write
 
 3. Implicit return:
+Implicit return is a function receives values back without using the return keyword
 
 4. Ruby blocks:
+Ruby blocks is an anonymous function that is passed to a method to determine the result
 
 5. Ruby hashes:
+A Ruby hash is a collection of unique keys and their values, similar to an array but instead of the values being referenced by an index, the values are referenced by a unique key
